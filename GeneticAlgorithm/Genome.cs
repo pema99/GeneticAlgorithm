c739f9f35c28813a4449a3e346066347ee31557f
pema99 <pemamalling@gmail.com>
2019-08-27 20:35:49 +0200

    Crossover operators

--- GeneticAlgorithm/Genome.cs
+++ GeneticAlgorithm/Genome.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace GeneticAlgorithm
 {
@@ -13,18 +14,74 @@ namespace GeneticAlgorithm
             this.Biases = biases;
         }
 
+        public Genome(params int[] layers)
+        {
+            if (layers.Length < 2)
+            {
+                throw new ArgumentOutOfRangeException("Network must have a minimum of 2 layers.");
+            }
+
+            int sumW = 0, sumB = 0;
+            for (int i = 1; i < layers.Length; i++)
+            {
+                sumW += layers[i] * layers[i - 1];
+                sumB += layers[i];
+            }
+
+            this.Weights = Util.Rand.NextDoubleArray(sumW);
+            this.Biases = new double[sumB];
+        }
+
+        public (Genome childA, Genome childB) WholeArithmeticCrossover(Genome other, double bias = 0.5)
+        {
+            (Genome childA, Genome childB) result = (new Genome(new double[Weights.Length], new double[Biases.Length]), new Genome(new double[Weights.Length], new double[Biases.Length]));
+
+            for (int i = 0; i < Weights.Length; i++)
+            {
+                result.childA.Weights[i] = bias * Weights[i] + (1.0 - bias) * other.Weights[i];
+                result.childA.Weights[i] = bias * other.Weights[i] + (1.0 - bias) * Weights[i];
+            }
+
+            for (int i = 0; i < Biases.Length; i++)
+            {
+                result.childA.Biases[i] = bias * Biases[i] + (1.0 - bias) * other.Biases[i];
+                result.childA.Biases[i] = bias * other.Biases[i] + (1.0 - bias) * Biases[i];
+            }
+
+            return result;
+        }
+
+        public (Genome childA, Genome childB) UniformCrossover(Genome other, double bias = 0.5)
+        {
+            (Genome childA, Genome childB) result = (new Genome(new double[Weights.Length], new double[Biases.Length]), new Genome(new double[Weights.Length], new double[Biases.Length]));
+
+            for (int i = 0; i < Weights.Length; i++)
+            {
+                result.childA.Weights[i] = alpha * Weights[i] + (1.0 - alpha) * other.Weights[i];
+                result.childA.Weights[i] = alpha * other.Weights[i] + (1.0 - alpha) * Weights[i];
+            }
+
+            for (int i = 0; i < Biases.Length; i++)
+            {
+                result.childA.Biases[i] = alpha * Biases[i] + (1.0 - alpha) * other.Biases[i];
+                result.childA.Biases[i] = alpha * other.Biases[i] + (1.0 - alpha) * Biases[i];
+            }
+
+            return result;
+        }
+
         public override string ToString()
         {
             string result = "Weights: { ";
             foreach (double weight in Weights)
             {
-                result += string.Format("{0:N4}", weight) + ", ";
+                result += string.Format("{0:N4}", weight) + "; ";
             }
             result += "}\n";
             result += "Biases: { ";
             foreach (double bias in Biases)
             {
-                result += string.Format("{0:N4}", bias) + ", ";
+                result += string.Format("{0:N4}", bias) + "; ";
             }
             result += "}";
             return result;
--- GeneticAlgorithm/Program.cs
+++ GeneticAlgorithm/Program.cs
@@ -6,15 +6,44 @@ namespace GeneticAlgorithm
     {
         static void Main(string[] args)
         {
+            new Program().Start();
+        }
+
+        public void Start()
+        {
             Network n = new Network(2, 5, 1);
 
-            var gen = n.ExtractGenome();
-            Console.WriteLine(gen);
+            TestXOR(n);
 
-            n.InsertGenome(gen);
-            Console.WriteLine(gen);
+            Func<Network, double> fitness = (Network net) =>
+            {
+                double score = 0;
+
+                score += Math.Abs(1.0 / n.Eval(new double[] { 0, 0 })[0]);
+                score += Math.Abs(1.0 / (n.Eval(new double[] { 0, 1 })[0] - 1));
+                score += Math.Abs(1.0 / (n.Eval(new double[] { 1, 0 })[0] - 1));
+                score += Math.Abs(1.0 / n.Eval(new double[] { 0, 0 })[0]);
+
+                return score;
+            };
+
+            Console.WriteLine(fitness(n));//new Genome(2, 5, 1));
+
+            //Console.WriteLine(n.ExtractGenome());
 
             Console.ReadKey();
+        }
+
+        public void TestXOR(Network n)
+        {
+            var test1 = n.Eval(new double[] { 0, 0 })[0];
+            Console.WriteLine("Input: 0 0, Got: {0}, Expected: 0", test1);
+            var test2 = n.Eval(new double[] { 0, 1 })[0];
+            Console.WriteLine("Input: 0 1, Got: {0}, Expected: 1", test2);
+            var test3 = n.Eval(new double[] { 1, 0 })[0];
+            Console.WriteLine("Input: 1 0, Got: {0}, Expected: 1", test3);
+            var test4 = n.Eval(new double[] { 1, 1 })[0];
+            Console.WriteLine("Input: 1 1, Got: {0}, Expected: 0", test4);
         }
     }
 }
